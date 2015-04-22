2d6ae803e11508bb706960c711a7296d166855aa
map32 <freepina@gmail.com>
2015-04-22 13:01:23 -0400

    kill all trees

---
+++ 12killalltrees/TreeNode.java
@@ -0,0 +1,55 @@
+public class TreeNode<T>{
+    T data;
+    TreeNode<T> left=null;
+    TreeNode<T> right=null;
+    
+    public TreeNode(T d, TreeNode<T> l, TreeNode<T> r){
+	this(d,l);
+	right = r;
+    }
+
+    public TreeNode(T d, TreeNode<T> l){
+	this(d);
+	left = l;
+    }
+
+    public TreeNode(T d){
+	data = d;
+    }
+
+    public T get(){
+	return data;
+    }
+
+    public TreeNode<T> getLeft(){
+	return left;
+    }
+
+    public TreeNode<T> getRight(){
+	return right;
+    }
+
+    public void set(T d){
+	data = d;
+    }
+    
+    public void setLeft(TreeNode<T> l){
+	left = l;
+    }
+
+    public void setRight(TreeNode<T> r){
+	right = r;
+    }
+
+    public boolean hasNext(){
+	return (hasLeft() || hasRight());
+    }
+
+    public boolean hasLeft(){
+	return left != null;
+    }
+    
+    public boolean hasRight(){
+	return right != null;
+    }
+}
