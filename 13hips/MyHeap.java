2ccf956fa20a1e6e67e2a5ca1a65677444e8a5f0
map32 <freepina@gmail.com>
2015-05-14 13:10:37 -0400

    stuff

--- 13hips/MyHeap.java
+++ 13hips/MyHeap.java
@@ -59,6 +59,7 @@ public class MyHeap {
     private boolean type;
     private int height, index;
     private HNode root = null;
+    private HNode curr = null;
 
     public MyHeap(){
 	this(true);
@@ -72,39 +73,65 @@ public class MyHeap {
     }
 
     public void add(int d){
+	add(d,root,1);
 	index++;
 	if(index==Math.pow(2,height)){
 	    index=0;
 	    height++;
-	    //System.out.println(height);
-	}
-	//System.out.println(index);
-	if(root==null){
-	    root = new HNode(d);
-	} else {
-	    traverse(d, root, 1);
 	}
     }
 
-    public HNode traverse(int d, HNode n, int localheight){
-	System.out.println(""+localheight+","+height);
-	if(localheight-1==height){
+    public boolean add(int d, HNode n, int lheight){
+	if(n==null){
+	    if(lheight == height + 1){
+		n = new HNode(d);
+		curr = n;
+		return true;
+	    } else {
+		return false;
+	    }
+	} else {
+	    if(traverse(d,n.getL(),lheight+1)){
+		if(height == lheight){
+		    n.setL(curr);
+		}
+		swap(n);
+		return true;
+	    } else if (traverse(d,n.getR(),lheight+1)){
+		if(height == lheight){
+		    n.setR(curr);
+		}
+		swap(n);
+		return true;
+	    }
+	    return false;
+	}
+    }
+
+    public boolean traverse(int d, HNode n, int localheight){
+	System.out.println(""+localheight+","+height+","+index);
+	if(localheight>height+1)
+	    return false;
+	if(localheight==height+1 && index <= Math.pow(2,height)){
 	    if(n==null){
 		n = new HNode(d);
+		curr = n;
+		return true;
 	    }
-	    return n;
 	} else {
-	    HNode c = traverse(d,n.getL(),localheight+1);
-	    if(c==null){
-		System.out.println("wat");
-		c = traverse(d,n.getR(),localheight+1);
-		n.setR(c);
-	    } else {
-		n.setL(c);
+	    if(traverse(d,n.getL(),localheight+1)){
+		if(localheight==height){
+		    n.setL(curr);
+		    return true;
+		}
+	    } else if(traverse(d,n.getR(),localheight+1)){
+		if(localheight==height){
+		    n.setR(curr);
+		    return true;
+		}
 	    }
-	    swap(n);
-	    return c;
 	}
+	return false;
     }
 
     private void swap(HNode n){
